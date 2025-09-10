d1adab5e0602583ff35726a86e5ce3aac2f782b3
grzkas <furiosek0@wp.pl>
2025-09-10 17:20:14 +0200

    Update z2_Kasubowski_zewnetrzny.js

--- z2_Kasubowski_zewnetrzny.js
+++ z2_Kasubowski_zewnetrzny.js
@@ -1,2 +1,3 @@
-document.write("ostatnia modyfikacja strony".fontcolor("fuchsia").textdecor.bold().fontsize(7)+"<br>"); 
+document.write("ostatnia modyfikacja strony".fontcolor("fuchsia").textdecor.bold().fontsize(7)."<br>"); 
 document.write(document.lastModified);
+
